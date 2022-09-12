76e6f7cca0cbdc98073209010108571863515402
domjos1994 <developing@domjos.de>
2022-09-12 19:48:59 +0200

    update app: solve bugs with gift

--- app/src/main/java/de/domjos/gift_app/fragments/ResultFragment.kt
+++ app/src/main/java/de/domjos/gift_app/fragments/ResultFragment.kt
@@ -7,8 +7,8 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import de.domjos.gift_app.customControls.GiftItem
 import de.domjos.gift_app.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
@@ -17,6 +17,7 @@ class ResultFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var all = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,46 +32,71 @@ class ResultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        updateQuestions(false)
+
+        _binding?.cmdResultAll?.setOnClickListener {
+            all = !all
+
+            updateQuestions(all)
+        }
+    }
+
+    fun updateQuestions(all: Boolean) {
         val ll: LinearLayout = _binding!!.llMain
-        for(i in 0..14) {
-            ll.getChildAt(i).visibility = GONE
-        }
 
-        var item1 = -1
-        var item2 = -1
-        var item3 = -1
-        var index1 = -1
-        var index2 = -1
-        var index3 = -1
-        for(i in 0..14) {
-            val item = this.arguments?.getInt(i.toString()) ?: continue
-
-            if(item1 <= item) {
-                index3 = index2
-                item3 = item2
-                index2 = index1
-                item2 = item1
-                index1 = i
-                item1 = item
-            } else if(item2 <= item) {
-                index3 = index2
-                item3 = item2
-                index2 = i
-                item2 = item
-            } else if(item3 <= item) {
-                index3 = i
-                item3 = item
+        if(all) {
+            for(i in 0..14) {
+                try {
+                    this.arguments?.getInt(i.toString())
+                        ?.let { (ll.getChildAt(i) as GiftItem).setPoints(it) }
+                    ll.getChildAt(i).visibility = VISIBLE
+                } catch (ex: Exception) {}
             }
-        }
+        } else {
+            for(i in 0..14) {
+                try {
+                    this.arguments?.getInt(i.toString())
+                        ?.let { (ll.getChildAt(i) as GiftItem).setPoints(it) }
+                    ll.getChildAt(i).visibility = GONE
+                } catch (ex: Exception) {}
+            }
 
-        if(index1 != -1) {
-            ll.getChildAt(index1).visibility = VISIBLE
-        }
-        if(index2 !=-1) {
-            ll.getChildAt(index2).visibility = VISIBLE
-        }
-        if(index3 != -1) {
-            ll.getChildAt(index3).visibility = VISIBLE
+            var item1 = -1
+            var item2 = -1
+            var item3 = -1
+            var index1 = -1
+            var index2 = -1
+            var index3 = -1
+            for(i in 0..14) {
+                val item = this.arguments?.getInt(i.toString()) ?: continue
+
+                if(item1 <= item) {
+                    index3 = index2
+                    item3 = item2
+                    index2 = index1
+                    item2 = item1
+                    index1 = i
+                    item1 = item
+                } else if(item2 <= item) {
+                    index3 = index2
+                    item3 = item2
+                    index2 = i
+                    item2 = item
+                } else if(item3 <= item) {
+                    index3 = i
+                    item3 = item
+                }
+            }
+
+            if(index1 != -1) {
+                ll.getChildAt(index1).visibility = VISIBLE
+            }
+            if(index2 !=-1) {
+                ll.getChildAt(index2).visibility = VISIBLE
+            }
+            if(index3 != -1) {
+                ll.getChildAt(index3).visibility = VISIBLE
+            }
         }
     }
 }
